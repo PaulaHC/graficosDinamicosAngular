44bd0e1a857d3bb209a1f6423cf2715c3509ca8c
PaulaHCA <paulahca53@gmail.com>
2023-03-10 10:49:41 +0100

    Update:Graficosdinamicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Gráficos
+import { NgChartsConfiguration, NgChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { BarChartComponent } from './components/barras-chart/barras-chart.component';
+import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './components/radar-chart/radar-chart.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChartComponent,
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule.forRoot(),
+    NgChartsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NgChartsConfiguration, useValue: { generateColors: false }}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
